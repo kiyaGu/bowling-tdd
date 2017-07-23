23cdfcd5b98547cb2f18b4c7fa4fadc17217e436
snozza <andrewcsnead@gmail.com>
2017-07-23 14:06:01 +0100

    updated with spare

---
+++ bowling.test.js
@@ -0,0 +1,33 @@
+const BowlingGame = require('./bowling');
+
+describe('Bowling Game should', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new BowlingGame(); 
+  });
+
+  function rollMany(times, pins) {
+    for (let i = 0; i < times; i++) {
+      game.roll(pins);
+    }
+  }
+
+  test('score a gutter game (all 0 rolls)', () => {
+    rollMany(20, 0);
+    const score = game.score();
+    expect(score).toBe(0);
+  });
+
+  test('score a game with all 1s for rolls', () => {
+    rollMany(20, 1);
+    const score = game.score();
+    expect(score).toBe(20);
+  });
+
+  test('handle a spare', () => {
+    expect(game.isSpare(5, 5)).toBeTruthy();
+    expect(game.isSpare(5, 4)).toBeFalsy();
+  });
+
+});
