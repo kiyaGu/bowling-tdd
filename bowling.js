a5a0c69f6570634f342f39da6b98a424163a8a01
KiyaG <hamilee.kiya@gmail.com>
2017-08-01 14:28:56 +0100

    wrote method to calculate strike bonus and modified the score method

--- bowling.js
+++ bowling.js
@@ -12,17 +12,40 @@ BowlingGame.prototype.isStrike = function(roll){
 BowlingGame.prototype.calculateSpareBonus = function(frameIndex){
   return this.rolls[frameIndex + 2];
 }
-BowlingGame.prototype.roll = function(pins) {
-  this.rolls.push(pins);
+BowlingGame.prototype.calculateStrikeBonus = function(frameIndex){
+  if(frameIndex !== 19){
+    if(this.rolls[frameIndex + 2] === 10){
+      //if the next roll is also a strike, for bonus add rolls from the next two frames
+      return this.rolls[frameIndex + 2] + this.rolls[frameIndex + 4];
+    } else {
+      //if the next roll is not strike add the two rolls in that frame
+      return this.rolls[frameIndex + 2] + this.rolls[frameIndex + 3];
+    }
+  }
 }
-
+BowlingGame.prototype.roll = function(pins) {
+  if(pins === 10){
+    //if it is strike make the next rolls to equal to zero
+    //as each frame is considered to have two rolls => total of 20 rolls
+    this.rolls.push(pins);
+    this.rolls.push(0);
+  } else{
+    this.rolls.push(pins);
+  }
+}
 BowlingGame.prototype.score = function() {
   let sum = 0;
-
   for (let i = 0; i < this.rolls.length; i++) {
-    sum += this.rolls[i];
+    let bonus = 0;
+    if(i <= 17){//not to include the 10th frame in calculating bonus
+      if(this.isStrike(this.rolls[i])){
+        bonus = this.calculateStrikeBonus(i);
+      } else if(this.isSpare(this.rolls[i],this.rolls[i+1])){
+        bonus = this.calculateSpareBonus(i);
+      }
+    }
+    sum += this.rolls[i] + bonus;
   }
   return sum;
 }
-
 module.exports = BowlingGame;
