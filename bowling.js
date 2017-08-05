7f2f99110cc077296b9e8b64a9487ab79f3e76c6
KiyaG <hamilee.kiya@gmail.com>
2017-08-05 07:27:59 +0100

    made the modifications based on the feedback

--- bowling.js
+++ bowling.js
@@ -1,51 +1,46 @@
 function BowlingGame() {
-  this.rolls = [];
+    this.rolls = [];
 }
 
 BowlingGame.prototype.isSpare = function(roll1, roll2) {
-  return roll1 + roll2 === 10;
+    return roll1 + roll2 === 10;
 }
-BowlingGame.prototype.isStrike = function(roll){
-  return roll === 10;
+BowlingGame.prototype.isStrike = function(roll) {
+    return roll === 10;
 }
 
-BowlingGame.prototype.calculateSpareBonus = function(frameIndex){
-  return this.rolls[frameIndex + 2];
+BowlingGame.prototype.calculateSpareBonus = function(frameIndex) {
+    //add the first roll in the next frame
+    return this.rolls[frameIndex + 2];
 }
-BowlingGame.prototype.calculateStrikeBonus = function(frameIndex){
-  if(frameIndex !== 19){
-    if(this.rolls[frameIndex + 2] === 10){
-      //if the next roll is also a strike, for bonus add rolls from the next two frames
-      return this.rolls[frameIndex + 2] + this.rolls[frameIndex + 4];
-    } else {
-      //if the next roll is not strike add the two rolls in that frame
-      return this.rolls[frameIndex + 2] + this.rolls[frameIndex + 3];
-    }
-  }
+
+BowlingGame.prototype.calculateStrikeBonus = function(frameIndex) {
+    //add the next two rolls
+    return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
 }
+
 BowlingGame.prototype.roll = function(pins) {
-  if(pins === 10){
-    //if it is strike make the next rolls to equal to zero
-    //as each frame is considered to have two rolls => total of 20 rolls
     this.rolls.push(pins);
-    this.rolls.push(0);
-  } else{
-    this.rolls.push(pins);
-  }
 }
+
 BowlingGame.prototype.score = function() {
-  let sum = 0;
-  for (let i = 0; i < this.rolls.length; i++) {
-    let bonus = 0;
-    if(i <= 17){//not to include the 10th frame in calculating bonus
-      if(this.isStrike(this.rolls[i])){
-        bonus = this.calculateStrikeBonus(i);
-      } else if(this.isSpare(this.rolls[i],this.rolls[i+1])){
-        bonus = this.calculateSpareBonus(i);
-      }
+    let sum = 0;
+    let index = 0;
+    for (let i = 1; i < 11; i++) {
+        if (this.isStrike(this.rolls[index])) {
+            // add the strike in the current frame and strike bonus
+            sum += this.rolls[index] + this.calculateStrikeBonus(index);
+            index++; // move to the next frame
+        } else if (this.isSpare(this.rolls[index], this.rolls[index + 1])) {
+            //add the two rolls in the current frame and the bonus for spare
+            sum += this.rolls[index] + this.rolls[index + 1] + this.calculateSpareBonus(index);
+            index += 2; // move to the next frame
+        } else {
+            //add the two rolls in the current frame
+            sum += this.rolls[index] + this.rolls[index + 1];
+            index += 2; // move to the next frame
+        }
     }
-    sum += this.rolls[i] + bonus;
-  }
-  return sum;
+    return sum;
 }
 module.exports = BowlingGame;
--- bowling.test.js
+++ bowling.test.js
@@ -1,69 +1,69 @@
 const BowlingGame = require('./bowling');
 
 describe('Bowling Game should', () => {
-  let game;
+    let game;
 
-  beforeEach(() => {
-    game = new BowlingGame();
-  });
+    beforeEach(() => {
+        game = new BowlingGame();
+    });
 
-  function rollMany(times, pins) {
-    for (let i = 0; i < times; i++) {
-      game.roll(pins);
+    function rollMany(times, pins) {
+        for (let i = 0; i < times; i++) {
+            game.roll(pins);
+        }
     }
-  }
 
-  test('score a gutter game (all 0 rolls)', () => {
-    rollMany(20, 0);
-    const score = game.score();
-    expect(score).toBe(0);
-  });
+    test('score a gutter game (all 0 rolls)', () => {
+        rollMany(20, 0);
+        const score = game.score();
+        expect(score).toBe(0);
+    });
 
-  test('score a game with all 1s for rolls', () => {
-    rollMany(20, 1);
-    const score = game.score();
-    expect(score).toBe(20);
-  });
+    test('score a game with all 1s for rolls', () => {
+        rollMany(20, 1);
+        const score = game.score();
+        expect(score).toBe(20);
+    });
 
-  test('handle a spare', () => {
-    expect(game.isSpare(5, 5)).toBeTruthy();
-    expect(game.isSpare(5, 4)).toBeFalsy();
-  });
+    test('handle a spare', () => {
+        expect(game.isSpare(5, 5)).toBeTruthy();
+        expect(game.isSpare(5, 4)).toBeFalsy();
+    });
 
-  test('handle a strike', () =>{
-    expect(game.isStrike(10)).toBeTruthy();
-    expect(game.isStrike(4)).toBeFalsy();
-  });
+    test('handle a strike', () => {
+        expect(game.isStrike(10)).toBeTruthy();
+        expect(game.isStrike(4)).toBeFalsy();
+    });
 
-  test('handle a bonus for spare', () => {
-    rollMany(2, 5);
-    game.roll(4);
-    let bonus = game.calculateSpareBonus(0);
-    expect(bonus).toBe(4)
-  });
-  test('handle a bonus for strike', () => {
-    rollMany(1, 10);
-    game.roll(8);
-    game.roll(6);
-    let bonus = game.calculateStrikeBonus(0);
-    expect(bonus).toBe(14)
-  });
-  test('calculates the score', () => {
-    game.roll(10);
-    game.roll(9);
-    game.roll(1);
-    rollMany(2,5);
-    game.roll(7);
-    game.roll(2);
-    rollMany(3,10);
-    game.roll(9);
-    game.roll(0);
-    game.roll(8);
-    game.roll(2);
-    game.roll(9);
-    game.roll(1);
-    game.roll(10);
-    let score = game.score();
-    expect(score).toBe(187)
-  });
+    test('handle a bonus for spare', () => {
+        rollMany(2, 5);
+        game.roll(4);
+        const bonus = game.calculateSpareBonus(0);
+        expect(bonus).toBe(4)
+    });
+    test('handle a bonus for strike', () => {
+        rollMany(1, 10);
+        game.roll(8);
+        game.roll(6);
+        const bonus = game.calculateStrikeBonus(0);
+        expect(bonus).toBe(14)
+    });
+    test('calculates the score', () => {
+        game.roll(10);
+        game.roll(9);
+        game.roll(1);
+        rollMany(2, 5);
+        game.roll(7);
+        game.roll(2);
+        rollMany(3, 10);
+        game.roll(9);
+        game.roll(0);
+        game.roll(8);
+        game.roll(2);
+        game.roll(9);
+        game.roll(1);
+        game.roll(10);
+        const score = game.score();
+        expect(score).toBe(187)
+    });
 });
